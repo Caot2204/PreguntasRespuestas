bb33ddb33935f90420f5e615ee576bae66e320c1
thynix <thynix@thynix-pc>
2020-01-30 00:11:56 -0600

    Se creo la interfaz RMI para los usuarios

--- Source/Servidor/src/mx/pr/comunicacion/interfacesrmi/UsuarioInterfaceRMI.java
+++ Source/Servidor/src/mx/pr/comunicacion/interfacesrmi/UsuarioInterfaceRMI.java
@@ -16,10 +16,63 @@
  */
 package mx.pr.comunicacion.interfacesrmi;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import mx.pr.comunicacion.objetoscliente.UsuarioCliente;
+
 /**
+ * Provee al cliente los métodos disponibles a usar relacionados con las cuentas
+ * de usuario dentro del servidor del juego
  *
+ * @version 1.0 29 01 2020
  * @author Carlos Onorio
  */
-public interface UsuarioInterfaceRMI {
-    
+public interface UsuarioInterfaceRMI extends Remote {
+
+    /**
+     * Almacena los datos de una cuenta de usuario en el servidor del juego.
+     *
+     * @param usuario Objeto con los datos del nuevo usuario, recibido desde un
+     * cliente
+     * @return true si se guardó exitosamente, false si no fué así
+     * @throws RemoteException lanzada si ocurre algún problema en la conexión
+     * cliente-servidor
+     */
+    boolean registrarUsuario(UsuarioCliente usuario) throws RemoteException;
+
+    /**
+     * Modifica los datos de un usuario registrado en el servidor del juego
+     *
+     * @param usuario Objeto con los datos actualizados de un usuario, recibido
+     * desde el cliente
+     * @return true si se actualizó correctamente, false si no fue así
+     * @throws RemoteException lanzada si ocurre algún problema en la conexión
+     * cliente-servidor
+     */
+    boolean actualizarUsuario(UsuarioCliente usuario) throws RemoteException;
+
+    /**
+     * Crea una nueva sesíon de un usuario en el servidor, si el correo y
+     * contraseña son correctos
+     *
+     * @param correo Correo electrónico de la cuenta de usuario
+     * @param contrasenia Contraseña de la cuenta de usuario
+     * @return true si se inició correctamente la sesión, false si los datos son
+     * incorrectos
+     * @throws RemoteException lanzada si ocurre algún problema en la conexión
+     * cliente-servidor
+     */
+    boolean iniciarSesion(String correo, String contrasenia) throws RemoteException;
+
+    /**
+     * Elimina del servidor la sesión de usuario que coincida con el correo
+     * recibido del cliente
+     *
+     * @param correo Correo electrónico de la cuenta de usuario
+     * @return true si se cerró la sesión, false si ocurrió algún problema
+     * @throws RemoteException lanzada si ocurre algún problema en la conexión
+     * cliente-servidor
+     */
+    boolean cerrarSesion(String correo) throws RemoteException;
+
 }
